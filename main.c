a89afefb65eb2abdf480958f4618ec234874e2ff
Rogelio Gudino <cananito@gmail.com>
2025-04-18 23:47:24 -0700

    Adds TODO.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include "calc.h"
 
 int main(void) {
+  // TODO: Hardcoding for now, need to mplement command and input parsing!
   char* weight = calc_plates_to_weight("45,45");
   printf("%s\n", weight);
   return 0;
