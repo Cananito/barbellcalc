adc07be865c31e269ca3aa23126240677f001724
Rogelio Gudino <cananito@gmail.com>
2026-02-12 12:47:08 -0800

    Changes main layout from XML to code.

--- android/src/com/cananito/barbellcalc/MainActivity.java
+++ android/src/com/cananito/barbellcalc/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cananito.barbellcalc;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
   public native void lala();
@@ -13,7 +16,15 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    RelativeLayout relativeLayout = new RelativeLayout(this);
+    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+        RelativeLayout.LayoutParams.WRAP_CONTENT,
+        RelativeLayout.LayoutParams.WRAP_CONTENT);
+    TextView textView = new TextView(this);
+    textView.setText("barbellcalc");
+    textView.setLayoutParams(layoutParams);
+    relativeLayout.addView(textView);
+    setContentView(relativeLayout);
     lala();
   }
 }
