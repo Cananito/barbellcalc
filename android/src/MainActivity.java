a0f0c60aeccbbdd7d4795b4de25efe688a2451a0
Rogelio Gudino <cananito@gmail.com>
2026-02-15 10:31:16 -0800

    Correct layout for the UI. Next up is styling.

--- android/src/MainActivity.java
+++ android/src/MainActivity.java
@@ -3,7 +3,9 @@ package com.cananito.barbellcalc;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.RelativeLayout;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -18,15 +20,39 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
-    RelativeLayout relativeLayout = new RelativeLayout(this);
-    RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-        RelativeLayout.LayoutParams.WRAP_CONTENT,
-        RelativeLayout.LayoutParams.WRAP_CONTENT);
-    TextView textView = new TextView(this);
-    textView.setText("barbellcalc");
-    textView.setLayoutParams(layoutParams);
-    relativeLayout.addView(textView);
-    setContentView(relativeLayout);
+    LinearLayout linearLayout = new LinearLayout(this);
+    linearLayout.setOrientation(LinearLayout.VERTICAL);
+    linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.MATCH_PARENT,
+        LinearLayout.LayoutParams.MATCH_PARENT));
+
+    TextView platesTextView = new TextView(this);
+    platesTextView.setText("Plates (lbs):");
+    platesTextView.setLayoutParams(new LayoutParams(
+        LayoutParams.MATCH_PARENT,
+        LayoutParams.WRAP_CONTENT));
+
+    EditText platesEditText = new EditText(this);
+    platesEditText.setLayoutParams(new LayoutParams(
+        LayoutParams.MATCH_PARENT,
+        LayoutParams.WRAP_CONTENT));
+
+    TextView weightTextView = new TextView(this);
+    weightTextView.setText("Weight (lbs):");
+    weightTextView.setLayoutParams(new LayoutParams(
+        LayoutParams.MATCH_PARENT,
+        LayoutParams.WRAP_CONTENT));
+
+    EditText weightEditText = new EditText(this);
+    weightEditText.setLayoutParams(new LayoutParams(
+        LayoutParams.MATCH_PARENT,
+        LayoutParams.WRAP_CONTENT));
+
+    linearLayout.addView(platesTextView);
+    linearLayout.addView(platesEditText);
+    linearLayout.addView(weightTextView);
+    linearLayout.addView(weightEditText);
+    setContentView(linearLayout);
 
     String plates = weightToPlates("255");
     String weight = platesToWeight("45,45");
